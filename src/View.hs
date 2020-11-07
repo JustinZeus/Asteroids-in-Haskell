0e1f4fd64d97e7540b6ed799a05e4781d9bb4a8c
SanazNajiyan <45420543+SanazNajiyan@users.noreply.github.com>
2020-11-07 11:30:39 +0100

    last update

--- src/View.hs
+++ src/View.hs
@@ -11,6 +11,15 @@ view = return . viewPure
 spaceship :: Picture
 spaceship = color white (polygon [(0,0),(-50,50),(-50,50),(0,0)])
 
+bullet :: Picture
+bullet = color white (Circle(20))
+
 position_sps :: Player -> Picture
+position_sps (Player (C(x,y) _) _ _) = translate x y spaceship
+
+position_bul :: Bullet -> Picture
+position_bul (B (C (x,y) _)) = translate x y bullet -- 
+
 
 viewPure :: GameState -> Picture
+viewPure gstate = pictures [position_sps (player gstate)]  --   ++[position_bul(bullets gstate)] ) this should be list of
